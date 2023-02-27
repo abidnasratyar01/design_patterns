997134fd035e316dca37f6f0d373dc464eea1d82
Ahmad Abid <abidnasratyar01@gmail.com>
2023-02-27 10:50:17 +0430

    Implementation of State Pattern

---
+++ statepattern/src/com/statepattern/pattern/Canvas.java
@@ -0,0 +1,25 @@
+package com.statepattern.pattern;
+
+public class Canvas {
+	private Tool currentTool;
+
+	public void mouseDown() 
+	{
+		currentTool.mouseDown();
+	}
+	
+	public void mouseUp()
+	{
+		currentTool.mouseUp();
+	}
+	
+	public Tool getCurrentTool() 
+	{
+		return currentTool;
+	}
+
+	public void setCurrentTool(Tool currentTool) 
+	{
+		this.currentTool = currentTool;
+	}
+}
